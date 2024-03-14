a71d44bc7c06a2a766c05f452db4a2a51c0bf60d
barai3045 <santosh.barai.045@gmail.com>
2024-03-14 23:53:45 +0600

    initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-48 bg-gradient-to-r from-black via-blue-900 to-black text-white font-semibold">
+      <div className="text-4xl">Main</div>
+    </div>
+  );
+}
